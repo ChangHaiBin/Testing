1bc727f37903403c6d3552373d5471c1926e3e08
HaiBin <haibinmath@gmail.com>
2016-05-02 21:02:10 -0400

    HELLO WORLD
    first commit

---
+++ StringScramble.java
@@ -0,0 +1,8 @@
+public class StringScramble { 
+    public boolean isScramble(String s1, String s2) { 
+        return false; 
+    } 
+    public static void main(String args[]){
+       System.out.println("HELLO WORLD");
+    }
+}
