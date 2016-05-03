75297f18982df582a5579e3bd81ded0e5b4711a7
HaiBin <haibinmath@gmail.com>
2016-05-02 21:14:07 -0400

    The most basic test case
    Unequal length

--- StringScramble.java
+++ StringScramble.java
@@ -1,8 +1,16 @@
 public class StringScramble { 
-    public boolean isScramble(String s1, String s2) { 
-        return false; 
+    public StringScramble(){
+        
+    }
+    public boolean isScramble(String s1, String s2) {
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        
+        return true; 
     } 
     public static void main(String args[]){
-       System.out.println("HELLO WORLD");
+       StringScramble SAMPLE=new StringScramble();
+       System.out.println(SAMPLE.isScramble("123123","456biaklsd"));
     }
 }
