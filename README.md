2935fbcbaaec5359c94b6b1c6c7511941b8e0342
HaiBin <haibinmath@gmail.com>
2016-05-02 22:25:44 -0400

    Simple test 2
    Testing missing characters from one string.

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ I will document my changes here.
 
 May 2nd: Testing out Git and HelloWorld.
 
+May 2nd: Simple test case checking length of string.
+
+May 2nd: Simple test case checking any character missing from one string, but present in another string.
--- StringScramble.java
+++ StringScramble.java
@@ -1,10 +1,37 @@
+import java.util.HashMap;
+import java.util.Set;
+
 public class StringScramble { 
     public StringScramble(){
         
     }
+    public void add(HashMap<Character,Integer> count, char temp, int X){
+        if(count.containsKey(temp)){
+            int K = count.get(temp);
+            count.put(temp,K+X);
+        }else{
+            count.put(temp,X);
+        }
+    }
     public boolean isScramble(String s1, String s2) {
+        /* Trivial Case*/
         if(s1.length() != s2.length()){
             return false;
+        }
+        
+        /* Test characters*/
+        HashMap<Character,Integer> count=new HashMap<Character,Integer>();
+        char temp = ' ';
+        for(int i = 0; i < s1.length(); i++){
+            temp = s1.charAt(i);
+            add(count,temp,1);
+            temp =s2.charAt(i);
+            add(count,temp,-1);
+        }
+        for(char test : count.keySet()){
+            if(count.get(test) != 0){
+                return false;
+            }
         }
         
         return true; 
@@ -12,5 +39,6 @@ public class StringScramble {
     public static void main(String args[]){
        StringScramble SAMPLE=new StringScramble();
        System.out.println(SAMPLE.isScramble("123123","456biaklsd"));
+       System.out.println(SAMPLE.isScramble("asdf","asdg"));
     }
 }
